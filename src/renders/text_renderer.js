f4ac404c8ab12a99f1bc680f53bbc757e56352e0
Jason Blanchard <jas.blanchard@gmail.com>
2015-07-19 12:32:15 -0400

    Added color

--- src/renders/text_renderer.js
+++ src/renders/text_renderer.js
@@ -1,6 +1,7 @@
 import LifeRunner from '../life_runner';
 import defaultSeed from '../default_seed';
 import readline from 'readline';
+import colors from 'colors';
 
 var rl = readline.createInterface({
   input: process.stdin,
@@ -17,7 +18,15 @@ let additionalSeed = [
   [8, 1],
   [9, 1],
   [9, 2],
-  [7, 1]
+  [7, 1],
+  [7, 3],
+  [7, 4],
+  [6, 4],
+  [5, 4],
+  [3, 9],
+  [3, 8],
+  [4, 8],
+  [4, 7]
 ];
 let seed = defaultSeed.concat(additionalSeed);
 
@@ -27,7 +36,7 @@ var world = LifeRunner.init({
   seed: seed
 });
 
-world.start(300, (state) => {
+world.start(100, (state) => {
   readline.cursorTo(rl, 0, 0)
   readline.clearScreenDown(rl)
   state.forEach((column) => {
@@ -35,7 +44,7 @@ world.start(300, (state) => {
       if (cell === 0) {
         process.stdout.write('[  ] ');
       } else {
-        process.stdout.write(`[${('0' + cell).slice(-2)}] `);
+        process.stdout.write(`[${('0' + cell).slice(-2)}] `.green);
       }
     });
     console.log('');
